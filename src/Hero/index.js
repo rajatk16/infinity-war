75f95805aa854c50b45e9b18716dd1fdae403b06
geekrajat <rajat.k16@gmail.com>
2018-09-05 02:29:56 -0500

    Update index.js

--- src/Hero/index.js
+++ src/Hero/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './style.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: whitesmoke;
+`;
 
 export default class Hero extends React.Component {
   render() {
     return (
-      <div className="Container">
+      <Container>
         {this.props.hero.name}
-      </div>
+      </Container>
     );
   }
 }
