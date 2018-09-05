232c8c672d13d47a01cf60bbfd43f1de143ea81f
geekrajat <rajat.k16@gmail.com>
2018-09-05 02:27:48 -0500

    Update index.js

--- src/Column/index.js
+++ src/Column/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import Hero from '../Hero';
-import './style.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 10px;
+  border: 1px solid lightgrey;
+  border-radius: 5px;
+  width: 33%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  padding: 10px;
+`;
+
+const HeroList = styled.div`
+  padding: 10px;
+  flex-grow: 1;
+  min-height: 100px;
+`;
 
 export default class Column extends React.Component {
   render() {
     return (
-      <div className="Container">
-        <div className="Title">{this.props.column.title}</div>
-        <div className="Hero-List">
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <Hero-List>
           {this.props.heroes.map(hero => <Hero key={hero.id} hero={hero} />)}
-        </div>
-      </div>
+        </Hero-List>
+      </Container>
     );
   }
 }
